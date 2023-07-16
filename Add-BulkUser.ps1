e52aac3253016c3fd861864fe9795b1f2468a4fe
shane <hartley.shane@outlook.com>
2023-07-16 13:00:25 -0400

    Converted file

---
+++ Add-BulkUser.ps1
@@ -0,0 +1,17 @@
+# Quickly Add a new AD user object from a CSV file. Specify a password, UPN, SamAccountName, and we use a boolean flag to enable the account
+
+$password = Read-Host "Type your password" -AsSecureString 
+
+Import-Csv .\users.csv |
+ForEach-Object {
+  $name = "$($_.LastName) $($_.FirstName)"
+   
+ New-ADUser -GivenName $_.FirstName -Surname $_.LastName `
+ -Name $name -SamAccountName $_.SamAccountName `
+ -UserPrincipalName "$($_.SamAccountName)@shane.local" `
+ -AccountPassword $password -Path "CN=Users,DC=Shane,DC=Local" `
+ -Enabled:$true
+}
+
+# CSV Columns: FirstName,LastName,SamAccountName
+
